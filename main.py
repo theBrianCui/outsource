a3416dc574f4df0f29f891039dbce9249855298c
Brian Cui <brian.cui@live.com>
2019-04-11 16:56:12 -0700

    Revert "Revert "more arguments""
    This reverts commit 4ac083c6e37ade6928d39fdbad13fab896432edc.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from utils import exec_sync, print_stdout
 
-exec_sync(["echo", "hello"], silent=False)
+exec_sync(["echo", "hello"],
+          "Running echo... ",
+          "echo failed!",
+          "Done.",
+          silent=False)
