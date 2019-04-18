f35806eadc7ba4f871bf59a4442fed4b6c9c3ffb
Mario <msf1013@gmail.com>
2019-04-18 17:31:27 -0500

    Add polling

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from utils import exec_sync, print_stdout
 import json
+import time
+import subprocess
 
 # PLEASE CHANGE THESE VARIABLES ACCORDINGLY. 
-rg = "rgsouth12"
-vm = "myvm12"
+rg = "rgsouth20"
+vm = "myvm20"
 
 # # az group create --name rgsouth --location southcentralus
 
@@ -44,10 +46,19 @@ print(str(ip) + ":" + str(port))
 
 # TODO: Make output go to file instead of NULL.
 # https://stackoverflow.com/questions/35327623/python-subprocess-run-a-remote-process-in-background-and-immediately-close-the-c
-s = exec_sync(["ssh", ip, "-o", "StrictHostKeyChecking=no", "nohup python -m SimpleHTTPServer %d >/dev/null 2>&1 &" % (port)],
+# ["ssh", ip, "-o", "StrictHostKeyChecking=no", "nohup python -m SimpleHTTPServer %d >/home/msf1013/logs 2>&1 &" % (port)]
+s = exec_sync(["ssh", ip, "-o", "StrictHostKeyChecking=no", "nohup python -m SimpleHTTPServer %d >/home/msf1013/logs 2>&1 &" % (port)],
           "Running echo... ",
           "echo failed!",
           "",
           capture_out=True)
 
-print(s)
+i = 1
+while True:
+     time.sleep(5)
+     process = subprocess.Popen("ssh %s -o StrictHostKeyChecking=no cat /home/msf1013/logs" % (ip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+     output,stderr = process.communicate()
+     status = process.poll()
+     print("Polling %d:" % (i))
+     print(output)
+     i += 1
