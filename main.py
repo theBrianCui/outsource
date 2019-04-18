1aee6a0bb96ed4124590cf966efb4d1ba865a5b7
Brian Cui <brian.cui@live.com>
2019-04-18 15:46:45 -0700

    simplify output flow

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import json
 import sys
 
 # PLEASE CHANGE THESE VARIABLES ACCORDINGLY. 
-RESOURCE_GROUP = "outsource_rgsouth"
-VIRTUAL_MACHINE = "outsource_vm"
+RESOURCE_GROUP = "outsource-rgsouth"
+VIRTUAL_MACHINE = "outsource-vm"
 
 # # az group create --name rgsouth --location southcentralus
 
 rg_exists = exec_sync("az group exists --name {}".format(RESOURCE_GROUP).split(" "),
             "Checking for existing resource group {}...".format(RESOURCE_GROUP),
-            "something went wrong.",
-            "Done.",
             capture_out=True, die=True).strip()
 
 if rg_exists == "true":
@@ -19,31 +17,21 @@ if rg_exists == "true":
 else:
     exec_sync(["az", "group", "create", "--name", RESOURCE_GROUP, "--location", "southcentralus"],
             "Creating resource group {}...".format(RESOURCE_GROUP),
-            "something went wrong.",
-            "Done.",
             capture_out=True, die=True)
 
 # az vm create --name myvm --resource-group rgsouth --image UbuntuLTS --generate-ssh-keys --size Standard_DS1_v2
-
 exec_sync(["az", "vm", "create", "--name", VIRTUAL_MACHINE, "--resource-group", RESOURCE_GROUP, "--image", "UbuntuLTS", "--generate-ssh-keys", "--size", "Standard_DS1_v2"],
-          "Running echo... ",
-          "echo failed!",
-          "",
+          "Creating virtual machine {}...".format(VIRTUAL_MACHINE),
           capture_out=True)
 
 # az vm open-port -g rgsouth -n myvm --port '*'
-
 exec_sync(["az", "vm", "open-port", "-g", RESOURCE_GROUP, "-n", VIRTUAL_MACHINE, "--port", "*"],
-          "Running echo... ",
-          "echo failed!",
-          "",
+          "Opening ports * ...",
           capture_out=True)
 
 # az vm list-ip-addresses --name myvm
 output = exec_sync(["az", "vm", "list-ip-addresses", "--name", VIRTUAL_MACHINE],
-          "Running echo... ",
-          "echo failed!",
-          "",
+          "Getting IP address...",
           capture_out=True)
 
 print("IP:PORT:")
@@ -54,10 +42,8 @@ print(str(ip) + ":" + str(port))
 
 # TODO: Make output go to file instead of NULL.
 # https://stackoverflow.com/questions/35327623/python-subprocess-run-a-remote-process-in-background-and-immediately-close-the-c
-s = exec_sync(["ssh", ip, "-o", "StrictHostKeyChecking=no", "nohup python -m SimpleHTTPServer %d >/dev/null 2>&1 &" % (port)],
-          "Running echo... ",
-          "echo failed!",
-          "",
+s = exec_sync(["ssh", ip, "-o", "StrictHostKeyChecking=no", "lsb_release -a"],
+          "Running command on remote VM...",
           capture_out=True)
 
 print(s)
