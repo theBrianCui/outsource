5b22b9afe1334861927d76ce73d25ad4475972f3
Brian Cui <brian.cui@live.com>
2019-04-23 16:26:19 -0700

    added basic argument parsing

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from utils import exec_sync, read_file_to_string, get_env, delete_file, write_string_to_file
-import email
 import json
-import time
 import subprocess
 import sys
+import time
+import argparse
+import shlex
+
 import ssh
+import email
+from utils import (delete_file, exec_sync, get_env, read_file_to_string,
+                   write_string_to_file)
+
+DESCRIPTION = "Outsource is a command line tool for running commands remotely."
+
+parser = argparse.ArgumentParser(description=DESCRIPTION)
+parser.add_argument('-e', '--email', nargs=1, help="Send an email to the specified address when the job completes.")
+parser.add_argument('args', nargs=argparse.REMAINDER)
+
+ARGUMENTS = parser.parse_args()
+ARGUMENT_PROGRAM = ""               # the base program, e.g. cowsay
+ARGUMENT_STRING_FULL = ""           # the full argument string, e.g. cowsay hello world
+for arg in ARGUMENTS.args:
+    if ARGUMENT_STRING_FULL == "":
+        ARGUMENT_PROGRAM = arg
+        ARGUMENT_STRING_FULL += arg
+    else:
+        ARGUMENT_STRING_FULL += " {}".format(shlex.quote(arg)) # escape arguments if necessary
+
+print(ARGUMENTS)
+print(ARGUMENT_STRING_FULL)
 
 # PLEASE CHANGE THESE VARIABLES ACCORDINGLY.
-SEND_EMAIL = False
-SEND_EMAIL_ADDRESS = "brianzcui" + "@gmail.com"
+SEND_EMAIL_ADDRESS = ARGUMENTS.email
 SEND_EMAIL_SCRIPT = "scripts/email.sh"
 
 RESOURCE_GROUP = "outsource-rgsouth"
@@ -59,7 +81,7 @@ ip = arr[0]["virtualMachine"]["network"]["publicIpAddresses"][0]["ipAddress"]
 port = 8000
 print(str(ip) + ":" + str(port))
 
-if SEND_EMAIL:
+if SEND_EMAIL_ADDRESS:
     try:
         ssh.run_remote_script(email.create_email_script(SEND_EMAIL_ADDRESS), ip)
     except Exception as e:
