31c3cbe855f67012296175f02d72d4a98eeb6eec
Brian Cui <brian.cui@live.com>
2019-05-07 16:21:51 -0700

    resource group environment variable

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from utils import (delete_file, exec_sync, get_env, read_file_to_string,
                    write_string_to_file)
 
 DESCRIPTION = "Outsource is a command line tool for running commands remotely."
-RESOURCE_GROUP = "outsource-rgsouth"
+RESOURCE_GROUP = os.getenv('OUTSOURCE_RESOURCE_GROUP', "outsource-rgsouth")
 
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 
